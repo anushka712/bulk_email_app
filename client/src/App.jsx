052f38086c85ecbef4b97c1922fc36fb51a6a23d
anushka712 <103297378+anushka712@users.noreply.github.com>
2025-11-18 20:49:48 +0545

    Project initialization with vite bundler and tailwind css.

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className=" flex justify-center items-center text-5xl text-red-500 h-screen">
+        Hello World!
+      </div>
+    </>
+  );
+}
+
+export default App;
