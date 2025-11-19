9478b720a51bf711aa173799b5094f99b357a606
anushka712 <103297378+anushka712@users.noreply.github.com>
2025-11-20 00:10:56 +0545

    Feat: User Login Functionality

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,9 @@ import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.jsx";
 import * as Mantine from "@mantine/core";
+import { ModalsProvider } from "@mantine/modals";
+
+import "@mantine/core/styles.css"; 
 
 const queryClient = new QueryClient();
 
@@ -11,7 +14,9 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Mantine.MantineProvider>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <ModalsProvider>
+          <App />
+        </ModalsProvider>
       </QueryClientProvider>
     </Mantine.MantineProvider>
   </StrictMode>
